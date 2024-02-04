2b24da0bb9270a12b3ff8269c3a8029f1ae4c45e
Jurayevkh <jurayevkh@gmail.com>
2024-02-05 01:39:31 +0500

    Interface ApplicationDbContext

---
+++ src/FindMosque.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FindMosque.Domain.Entities.Mosque;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindMosque.Application.Abstractions;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Mosque> Mosque { get; set; }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+}
+
