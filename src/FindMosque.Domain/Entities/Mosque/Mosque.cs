c4911074a77016def2fce34817104d5a91c99e65
Jurayevkh <jurayevkh@gmail.com>
2024-02-05 01:40:30 +0500

    Entities Created

---
+++ src/FindMosque.Domain/Entities/Mosque/Mosque.cs
@@ -0,0 +1,14 @@
+﻿namespace FindMosque.Domain.Entities.Mosque;
+
+public class Mosque:BaseEntity
+{
+    public string Name { get; set; }
+    public decimal Latitude { get; set; }
+    public decimal Longtitude { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string? PostCode { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
+
