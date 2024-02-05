6bf560521e147eb1c3766154a39dd581c30e0e53
Jurayevkh <jurayevkh@gmail.com>
2024-02-05 23:49:01 +0500

    Queries

---
+++ src/FindMosque.Application/UseCases/Mosque/Query/GetNearestMosqueQuery.cs
@@ -0,0 +1,10 @@
+﻿namespace FindMosque.Application.UseCases.Mosque.Query;
+using MediatR;
+using FindMosque.Domain.Entities.Mosque;
+
+public class GetNearestMosqueQuery:IRequest<Mosque>
+{
+    public required decimal Latitude { get; set; }
+    public required decimal Longtitude { get; set; }
+}
+
