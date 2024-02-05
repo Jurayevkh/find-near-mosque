0911ec87fe09b77bbf5ae95b34e8619c3846a0f9
Jurayevkh <jurayevkh@gmail.com>
2024-02-05 23:51:20 +0500

    Dependency Injection for reference layer

---
+++ src/FindMosque.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using FindMosque.Application.Abstractions;
+using FindMosque.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FindMosque.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<IApplicationDbContext, MosqueDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
+
