b68323bfd664d08c76ccb53ee6e9158a3953a073
Jurayevkh <jurayevkh@gmail.com>
2024-02-13 00:39:28 +0500

    Create and Update DTOs

---
+++ dotnet-6-version/FindMosque.API/DTO/Mosque/CreateMosqueDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace FindMosque.API.DTO.Mosque;
+
+public class CreateMosqueDTO
+{
+    public string Name { get; set; }
+    public decimal Latitude { get; set; }
+    public decimal Longtitude { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string? PostCode { get; set; }
+}
+
---
+++ dotnet-6-version/FindMosque.API/DTO/Mosque/UpdateMosqueDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FindMosque.API.DTO.Mosque;
+
+public class UpdateMosqueDTO
+{
+    public string OldName { get; set; }
+    public string Name { get; set; }
+    public decimal Latitude { get; set; }
+    public decimal Longtitude { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string? PostCode { get; set; }
+}
+
