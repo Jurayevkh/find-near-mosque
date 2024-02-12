97a102ad4580a4178abff2b92a0d8a00b6088fe8
Jurayevkh <jurayevkh@gmail.com>
2024-02-13 00:40:17 +0500

    DB

---
+++ dotnet-6-version/FindMosque.Infrastructure/Data/MosqueDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FindMosque.Application.Abstraction;
+using FindMosque.Domain.Entities.Mosque;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindMosque.Infrastructure.Data;
+
+public class MosqueDbContext:DbContext, IApplicationDbContext
+{
+    public MosqueDbContext(DbContextOptions<MosqueDbContext> options) : base(options)
+    { }
+
+    public DbSet<Mosque> Mosque { get; set; }
+}
+
